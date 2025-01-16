1436472623a22200b69e4382101b5dc07e6f9eef
Pascal Twoli <pascaltwoli@gmail.com>
2025-01-16 17:20:40 +0300

    initial commit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js")
+      .then(reg => console.log("Service Worker registered:", reg))
+      .catch(err => console.error("Service Worker registration failed:", err));
+  });
+}
+
 // Selecting elements
 const popUp = document.querySelector('.pop-up');
 const popUpCloser = document.querySelector('.close-icon');
